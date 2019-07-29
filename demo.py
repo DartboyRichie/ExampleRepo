5e9fe3658b44892a4145a9732db8c559c4d15f63
Richard Desir <mymac@Richards-MacBook-Air.local>
2019-07-29 10:25:38 -0700

    Added demo.py

---
+++ demo.py
@@ -0,0 +1,15 @@
+#----------------------------------------------------------
+##Name: Richie Desir 
+##Date: July 29, 2019
+##
+##Description: This is a sample python file,
+##            including a header and a main function
+#----------------------------------------------------------
+
+def main():
+
+    name = "Richard"
+    print("Hello, my name is", name)
+
+if __name__ == "__main__":
+    main()
